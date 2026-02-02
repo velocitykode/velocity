db4f564791067965377bb209c05982ac39df7fec
Ali Raza <brainlet.ali@gmail.com>
2026-02-02 15:48:20 +0500

    fix: Dispatch mail events from mail.Send()
    The mail.Send() function was not dispatching events, only Manager.Send()
    was. This meant applications using mail.Send() directly wouldn't get
    mail.sent/mail.failed events captured by APM tools like Velwatch.

--- pkg/mail/mail.go
+++ pkg/mail/mail.go
@@ -2,6 +2,7 @@ package mail
 
 import (
 	"context"
+	"time"
 )
 
 // Default mailer instance
@@ -10,7 +11,26 @@ var defaultMailer Mailer
 // Send sends an email using the default mailer
 func Send(ctx context.Context, msg *Message) error {
 	ensureInitialized()
-	return defaultMailer.Send(ctx, msg)
+
+	// Extract recipient emails for event dispatching
+	toAddresses := msg.GetTo()
+	toEmails := make([]string, len(toAddresses))
+	for i, addr := range toAddresses {
+		toEmails[i] = addr.Email
+	}
+	subject := msg.GetSubject()
+
+	start := time.Now()
+	err := defaultMailer.Send(ctx, msg)
+	duration := time.Since(start)
+
+	if err != nil {
+		dispatchMailFailed(ctx, toEmails, subject, "default", err, duration)
+		return err
+	}
+
+	dispatchMailSent(ctx, toEmails, subject, "default", duration)
+	return nil
 }
 
 // SetDefaultMailer sets the default mailer
